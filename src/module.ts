49a7802407ae58668d2380258247a9b13075b8dd
René Majewski <rene.majewski@gmx.de>
2025-03-21 11:55:31 +0100

    wip(lint): Config eslint and prettier

--- src/module.ts
+++ src/module.ts
@@ -1,29 +1,29 @@
-import { createResolver, defineNuxtModule, extendPages } from '@nuxt/kit'
-import {globbySync} from 'globby'
+import { createResolver, defineNuxtModule } from '@nuxt/kit';
+import { globbySync } from 'globby';
 
 export default defineNuxtModule({
   meta: {
-    "name": "@rene-majewski/website-module-admin",
-    "configKey": "moduleAdmin"
+    name: '@rene-majewski/website-module-admin',
+    configKey: 'moduleAdmin',
   },
-  setup(_, nuxt){
-    console.info("📦 @rene-majewski/website-module-admin geladen!")
+  setup(_, nuxt) {
+    console.info('📦 @rene-majewski/website-module-admin geladen!');
 
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
     nuxt.hook('pages:extend', (pages) => {
-      const modulePages = globbySync(['runtime/pages/**/*.vue', 'runtime/pages/*.vue'], {cwd: __dirname})
+      const modulePages = globbySync(['runtime/pages/**/*.vue', 'runtime/pages/*.vue'], { cwd: __dirname });
 
       modulePages.forEach((file) => {
-        const relativePath = file.replace(/^runtime\/pages\//, '').replace(/\.vue/, '')
-        const routePath = relativePath === 'index' ? '' : `/${relativePath}`
+        const relativePath = file.replace(/^runtime\/pages\//, '').replace(/\.vue/, '');
+        const routePath = relativePath === 'index' ? '' : `/${relativePath}`;
 
         pages.push({
           name: `admin-${relativePath.replace(/\//g, '-')}`,
           path: `/admin${routePath}`,
-          file: resolver.resolve(file)
-        })
-      })
-    })
-  }
-})
+          file: resolver.resolve(file),
+        });
+      });
+    });
+  },
+});
