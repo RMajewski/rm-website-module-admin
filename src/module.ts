603fdd1f53b3cc778f049d7f9b9b26b8bb937ef4
René Majewski <rene.majewski@gmx.de>
2025-03-21 11:13:56 +0100

    wip(Pages): Add index page

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,29 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { createResolver, defineNuxtModule, extendPages } from '@nuxt/kit'
+import {globbySync} from 'globby'
 
 export default defineNuxtModule({
   meta: {
     "name": "@rene-majewski/website-module-admin",
-    "configKey": "@rene-majewski/website-module-admin"
+    "configKey": "moduleAdmin"
   },
-  setup() {
-    console.debug("📦 @rene-majewski/website-module-admin geladen!")
+  setup(_, nuxt){
+    console.info("📦 @rene-majewski/website-module-admin geladen!")
+
+    const resolver = createResolver(import.meta.url)
+
+    nuxt.hook('pages:extend', (pages) => {
+      const modulePages = globbySync(['runtime/pages/**/*.vue', 'runtime/pages/*.vue'], {cwd: __dirname})
+
+      modulePages.forEach((file) => {
+        const relativePath = file.replace(/^runtime\/pages\//, '').replace(/\.vue/, '')
+        const routePath = relativePath === 'index' ? '' : `/${relativePath}`
+
+        pages.push({
+          name: `admin-${relativePath.replace(/\//g, '-')}`,
+          path: `/admin${routePath}`,
+          file: resolver.resolve(file)
+        })
+      })
+    })
   }
 })
